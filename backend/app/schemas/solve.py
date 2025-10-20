764a71826d7027f0dd3b722751d0349358231fd6
ian-yeh <ianyeh7@gmail.com>
2025-10-19 22:45:34 -0400

    update tables

--- backend/app/schemas/solve.py
+++ backend/app/schemas/solve.py
@@ -3,10 +3,16 @@ from datetime import datetime
 
 class Solve(BaseModel):
     id: int
-    user_id: int
+    user_id: str
     time: int # stored in centiseconds
     scramble: str
     dnf: bool
     plusTwo: bool
     createdAt: datetime
     updatedAt: datetime
+
+class SolveResponese(BaseModel):
+    solve: Solve
+
+    class Config:
+        from_attributes = True
