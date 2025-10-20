43bff8da44611538afeb91f7b55af0bddf7173b5
ian-yeh <ianyeh7@gmail.com>
2025-10-19 23:21:30 -0400

    add: endpoint to add solves to db

--- backend/app/schemas/solve.py
+++ backend/app/schemas/solve.py
@@ -4,15 +4,21 @@ from datetime import datetime
 class Solve(BaseModel):
     id: int
     user_id: str
+    cube_type: str
     time: int # stored in centiseconds
     scramble: str
     dnf: bool
-    plusTwo: bool
     createdAt: datetime
-    updatedAt: datetime
 
-class SolveResponese(BaseModel):
+class SolveResponse(BaseModel):
     solve: Solve
 
     class Config:
         from_attributes = True
+
+class SolveRequest(BaseModel):
+    user_id: str
+    cube_type: str
+    time: int # stored in centiseconds
+    scramble: str
+    dnf: bool
