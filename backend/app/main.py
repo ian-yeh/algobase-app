5e110dd595d8da78853cc6fe5bc5e5460b4ac19d
ian-yeh <ianyeh7@gmail.com>
2025-10-22 12:52:51 -0400

    user update profile endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,6 +52,23 @@ def create_user(req: UserRequest, db = Depends(get_db)):
 
     return {"user": user}
 
+@app.put("/user", response_model=UserResponse)
+def update_user(req: UserRequest, db = Depends(get_db)):
+    user = db.query(User).filter(User.id == req.user_id).first()
+
+    if not user:
+        raise Exception("User not found")
+
+    user.username=req.username
+    user.email=req.email
+    user.imageUrl=req.imageUrl
+    user.lastActivityDate=datetime.now()
+
+    db.commit()
+    db.refresh(user)
+
+    return {"user": user}
+
 @app.post("/solve", response_model=SolveResponse)
 def create_solve(req: SolveRequest, db = Depends(get_db)):
     # Create new solve object
